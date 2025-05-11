6222bab842148240cfc5fc4fd80feaebbd9e00b3
subhashprasad2001@gmail.com <{96110162+subhashdippu@users.noreply.github.com}+{subhashdippu}@users.noreply.github.com>
2025-05-11 21:01:22 +0530

    adding tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        green: "#19207d",
+        red: "#FF6868",
+        secondary: "#555",
+        prigmayBG: "#FCFCFC",
+      },
+    },
+  },
+  plugins: [require("daisyui")],
+};
