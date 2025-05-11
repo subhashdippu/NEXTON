008aa3a7e443531d76721b86e39e4d38e8970cb3
subhashprasad2001@gmail.com <{96110162+subhashdippu@users.noreply.github.com}+{subhashdippu}@users.noreply.github.com>
2025-05-12 00:33:41 +0530

    adding redux

--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
+
+import { Provider } from "react-redux";
 import { RouterProvider } from "react-router-dom";
+
 import router from "./routers/router";
+import store from "./redux/store";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<RouterProvider router={router} />);
+
+root.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
+  </React.StrictMode>
+);
---
+++ src/redux/productSlice.js
@@ -0,0 +1,86 @@
+// src/redux/productSlice.js
+
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  products: [
+    {
+      id: 1,
+      title: "Product 1",
+      category: "Men’s fashion",
+      price: 120,
+      oldPrice: 200,
+      rating: 4.2,
+      reviews: 10,
+      images: [
+        "https://static8.depositphotos.com/1020341/896/i/950/depositphotos_8969502-stock-photo-human-face-with-cracked-texture.jpg",
+      ],
+      addedAt: "2024-01-01",
+    },
+    {
+      id: 2,
+      title: "Product 1",
+      category: "Men’s fashion",
+      price: 120,
+      oldPrice: 200,
+      rating: 4.2,
+      reviews: 10,
+      images: [
+        "https://static8.depositphotos.com/1020341/896/i/950/depositphotos_8969502-stock-photo-human-face-with-cracked-texture.jpg",
+      ],
+      addedAt: "2024-01-01",
+    },
+    {
+      id: 4,
+      title: "Product 1",
+      category: "Men’s fashion",
+      price: 120,
+      oldPrice: 200,
+      rating: 4.2,
+      reviews: 10,
+      images: [
+        "https://static8.depositphotos.com/1020341/896/i/950/depositphotos_8969502-stock-photo-human-face-with-cracked-texture.jpg",
+      ],
+      addedAt: "2024-01-01",
+    },
+    {
+      id: 5,
+      title: "Product 1",
+      category: "Men’s fashion",
+      price: 120,
+      oldPrice: 200,
+      rating: 4.2,
+      reviews: 10,
+      images: [
+        "https://static8.depositphotos.com/1020341/896/i/950/depositphotos_8969502-stock-photo-human-face-with-cracked-texture.jpg",
+      ],
+      addedAt: "2024-01-01",
+    },
+  ], // Hardcoded product data here
+  values: [100, 500], // Price range filter
+  selectedCats: [], // Selected categories filter
+  sortOrder: "Most Popular", // Sorting filter
+};
+
+const productSlice = createSlice({
+  name: "product",
+  initialState,
+  reducers: {
+    setProducts: (state, action) => {
+      state.products = action.payload; // Update products in the state
+    },
+    setPriceRange: (state, action) => {
+      state.values = action.payload;
+    },
+    setSelectedCats: (state, action) => {
+      state.selectedCats = action.payload;
+    },
+    setSortOrder: (state, action) => {
+      state.sortOrder = action.payload;
+    },
+  },
+});
+
+export const { setProducts, setPriceRange, setSelectedCats, setSortOrder } =
+  productSlice.actions;
+export default productSlice.reducer;
