f714b85ec3d7197fc96058098eac10cf6732a07e
subhashprasad2001@gmail.com <{96110162+subhashdippu@users.noreply.github.com}+{subhashdippu}@users.noreply.github.com>
2025-05-11 21:14:12 +0530

    adding initialize project skeleton

---
+++ src/pages/main.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { Outlet } from "react-router-dom";
+function HomePage() {
+  return (
+    <div>
+      {/* <Navbar /> */}
+      <Outlet />
+    </div>
+  );
+}
+export default HomePage;
